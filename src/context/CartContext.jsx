0308688b5e0a450b43e4e5e0a07e0b9e317e4b74
wanting <wantingpeng.cici@gmail>
2024-09-09 18:03:03 +0200

    problem:rewrite setCartList
    The setCartList function should be used to update the state,
     but now function just return a new array without actually calling
      the original setCartList to update CartList
    so we should name the logic as a new function 'addCartItem' and call it in setCartList
    
    now setCartList get the result from fuc 'addCartItem',can update CartList

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -11,6 +11,19 @@ export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartList, setCartList] = useState([]);
 
+  setCartList = (product) => {
+    const existingCartItem = cartList.find(
+      (cartitem) => cartitem.id === product.id
+    );
+    if (existingCartItem)
+      return [
+        ...cartList,
+        { ...existingCartItem, quantity: existingCartItem.quantity + 1 },
+      ];
+
+    return [...cartList, { ...product, quantity: 1 }];
+  };
+
   return (
     <CartContext.Provider
       value={{ isCartOpen, setIsCartOpen, cartList, setCartList }}
