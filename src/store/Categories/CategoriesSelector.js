98d2ba9f98d632398a1a12365d322bbf88f4e1e3
wanting <wantingpeng.cici@gmail>
2024-09-12 18:01:34 +0200

    reselect
    reduce((acc, current) return new a new reference each time,
    React assumes that something has changed, even if the actual contents are the same.
    
    reselect creats memoized selectors in Redux.It ensures that a selector recalculates
    its output only when the inputs change, preventing unnecessary re-renders.

--- src/store/Categories/CategoriesSelector.js
+++ src/store/Categories/CategoriesSelector.js
@@ -1,13 +1,26 @@
-export const selectCategories = (state) => {
-  console.log(state);
+import { createSelector } from "reselect";
 
-  const arr = state.Categories.categories;
-  return (
-    arr &&
-    arr.reduce((acc, current) => {
+const selectCategoriesReducer = (state) => state.Categories.categories; //get the target data from store tree, then we can process data later
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (
+    categories //categories:come from selectCategoriesReducer, it only run , when selectCategoriesReducer is different
+  ) => {
+    return categories.reduce((acc, current) => {
       const { title, items } = current;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
-  );
-};
+    }, {});
+  }
+);
+
+/* export const selectCategories = (state) => {
+  console.log(state);
+
+  const arr = state.Categories.categories;
+  return arr.reduce((acc, current) => {
+    const { title, items } = current;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {}); 
+};*/
