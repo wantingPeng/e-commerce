520c38a16ad319b4c57b11f9185a9e1a331a0eda
wanting <wantingpeng.cici@gmail>
2024-09-13 12:43:08 +0200

    redux-persist states Categories, Cart
    redux-persist manage and persist states in a web application across page reloads or browser sessions,
     by saving the Redux store in persistent storage, such as localStorage or sessionStorage, and rehydrating it when the application is reloaded.

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,22 @@
 import { compose, legacy_createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./rootReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //Storage Engines: localStorage
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["user"], // which state, you want to persist or not persist, here the current will betracked by AuthstateLister, so we don't have to persist it, otherweise may cause conflict
+};
+const PersistReducer = persistReducer(persistConfig, rootReducer); // (config, reducer)  returns an enhanced reducer
+
 const middlewares = [logger]; //catch actions before they hit our reducer and they log out the state
 const composedEnhance = compose(applyMiddleware(...middlewares));
+
 export const store = legacy_createStore(
-  rootReducer,
+  PersistReducer, //new the saved value is persisted
   undefined,
   composedEnhance
 );
+export const persistor = persistStore(store); //returns persistor object
