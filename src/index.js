36df2f20756fdc571f338ecfe13fc666932283c1
wanting <wantingpeng.cici@gmail>
2024-09-13 21:43:34 +0200

    createSlice
    automatically generate action creators and type

--- src/index.js
+++ src/index.js
@@ -12,11 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,9 @@ const persistConfig = {
 };
 const PersistReducer = persistReducer(persistConfig, rootReducer); // (config, reducer)  returns an enhanced reducer
 
-const middlewares = [process.env.NODE_ENV!=='production' && logger].filter(Boolean); //catch actions before they hit our reducer and they log out the state
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+); //catch actions before they hit our reducer and they log out the state
 const composedEnhance = compose(applyMiddleware(...middlewares));
 
 export const store = legacy_createStore(
@@ -20,7 +22,3 @@ export const store = legacy_createStore(
   composedEnhance
 );
 export const persistor = persistStore(store); //returns persistor object
-
-
-[2===3 && {A:'STRING'}].filter(Boolean) return []
-[3===3 && {A:'STRING'}].filter(Boolean) return [{A:'STRING'}]
