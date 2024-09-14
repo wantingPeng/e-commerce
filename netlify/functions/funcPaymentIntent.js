db92055c9094aaf5ab90f5391756b216dd54c5e8
wanting <wantingpeng.cici@gmail>
2024-09-14 22:20:33 +0200

    creat paymentIntent at backend and pass amount sucessfully from fontend
    we have to creat netlify and functions folder, this is speacial for netlify enviroument

---
+++ netlify/functions/funcPaymentIntent.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECREAT_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { amount } = JSON.parse(event.body);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.error(error.message);
+    return {
+      status: 400,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+/* require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECREAT_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { amount } = JSON.parse(event.body);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log({ error });
+
+    return {
+      status: 400,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+ */
--- src/component/PaymentForm.jsx
+++ src/component/PaymentForm.jsx
@@ -1,11 +1,54 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import Button from "./Button";
 import { PaymentFormContainer, FormContainer } from "./PaymentForm.style";
-
+import { StripeCardElement } from "@stripe/stripe-js";
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if (!stripe || !elements) return;
+      const response = await fetch("/.netlify/functions/funcPaymentIntent", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ amount: 10000 }),
+      }).then((response) => response.json());
+      console.log(response);
+      console.log("ssssssssssssssssssss");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /* const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const response = await fetch("/.netlify/functions/funcPaymentIntent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 100 }),
+    }).then((res) => res.json());
+    console.log(response);
+    console.log("ssssssssssssssssssss");
+  }; */
+
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonContent="pay now"></Button>
